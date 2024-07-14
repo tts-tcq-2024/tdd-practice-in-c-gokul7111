f85210099ada36e8703cce074a17d3f1c7885f65
gokul7111 <gokulvadivel711@gmail.com>
2024-07-14 20:22:12 +0530

    Update StringCalculator.h

--- StringCalculator.h
+++ StringCalculator.h
@@ -1,3 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int sum_numbers(const char *numbers) {
+  
+    int sum = 0;
+    char *numbers_copy = strdup(numbers);
+    char *token = strtok(numbers_copy, ",");
+
+    while (token != NULL) {
+        sum += atoi(token);
+        token = strtok(NULL, ",");
+    }
+
+    free(numbers_copy); 
+    return sum;
+}
+
 int add(const char* input)
 {
   if(*input == '\0')
@@ -6,7 +25,7 @@ int add(const char* input)
   }
   else
   {
-    return 1;
+    sum_numbers(input);
   }
   
 }
