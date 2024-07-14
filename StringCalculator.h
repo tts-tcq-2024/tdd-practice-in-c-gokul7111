767e24977621f81f0d38442dcac51f89d7a09096
gokul7111 <gokulvadivel711@gmail.com>
2024-07-14 21:53:40 +0530

    Update StringCalculator.h

--- StringCalculator.h
+++ StringCalculator.h
@@ -2,31 +2,83 @@
 #include <stdlib.h>
 #include <string.h>
 
-int sum_numbers(const char *numbers) {
-  
-    int sum = 0;
-    char *numbers_copy = strdup(numbers);
-    char *token = strtok(numbers_copy, ",");
+#define MAX_NUMBERS 1000
+#define MAX_DELIMITER_LENGTH 10
 
-    while (token != NULL) {
-        sum += atoi(token);
-        token = strtok(NULL, ",");
+void throw_exception(int negatives[], int count) {
+    printf("Exception: Negatives not allowed: ");
+    for (int i = 0; i < count; i++) {
+        printf("%d", negatives[i]);
+        if (i < count - 1) {
+            printf(", ");
+        }
     }
-
-    free(numbers_copy); 
-    return sum;
+    printf("\n");
+    exit(1);
 }
 
-int add(const char* input)
-{
-  if(*input == '\0')
-  {
-    return 0;
-  }
-  else
-  {
-    sum_numbers(input);
-  }
-  
+void add_delimiters(const char *delimiter_start, const char *delimiter_end, char *delimiters) {
+    while (delimiter_start < delimiter_end) {
+        const char *end = strchr(delimiter_start, ']');
+        if (end == NULL || end > delimiter_end) break;
+        strncat(delimiters, delimiter_start, end - delimiter_start);
+        delimiter_start = end + 1;
+        if (*delimiter_start == '[') delimiter_start++;
+    }
+}
+
+void parse_delimiters(const char *numbers, char *delimiters, const char **num_start) {
+    if (strncmp(numbers, "//", 2) == 0) {
+        const char *delimiter_end = strstr(numbers, "\n");
+        if (delimiter_end != NULL) {
+            *num_start = delimiter_end + 1;
+            const char *delimiter_start = numbers + 2;
+            if (*delimiter_start == '[') {
+                delimiter_start++;
+                add_delimiters(delimiter_start, delimiter_end, delimiters);
+            } else {
+                delimiters[0] = *delimiter_start;
+                delimiters[1] = '\0';
+            }
+        }
+    }
+}
+
+void tokenize_and_sum(const char *num_start, const char *delimiters, int *sum, int *negatives, int *negative_count) {
+    char *numbers_copy = strdup(num_start);
+    char *token = strtok(numbers_copy, delimiters);
+
+    while (token != NULL) {
+        int num = atoi(token);
+        if (num < 0) {
+            negatives[(*negative_count)++] = num;
+        } else if (num <= 1000) {
+            *sum += num;
+        }
+        token = strtok(NULL, delimiters);
+    }
+
+    free(numbers_copy);
+}
+
+int add(const char *numbers) {
+    if (numbers == NULL || *numbers == '\0') {
+        return 0;
+    }
+
+    char delimiters[MAX_DELIMITER_LENGTH] = ",\n";
+    const char *num_start = numbers;
+    int sum = 0;
+    int negatives[MAX_NUMBERS];
+    int negative_count = 0;
+
+    parse_delimiters(numbers, delimiters, &num_start);
+    tokenize_and_sum(num_start, delimiters, &sum, negatives, &negative_count);
+
+    if (negative_count > 0) {
+        throw_exception(negatives, negative_count);
+    }
+
+    return sum;
 }
 
